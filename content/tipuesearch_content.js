a49eaef80ea375040d2659e21099e1801f586bc8
40871106 <40871106@gm.nfu.edu.tw>
2021-03-11 15:14:59 +0800

    make process

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': '黃翊銘的網站', 'tags': '', 'url': 'About.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': '黃翊銘的網站 \n 製作過程 \n \n 第一次大改版 \n', 'tags': '', 'url': 'About.html'}]};
